cc761bcb4e561431ccba22358c31c4723d32fdca
Martin Roznovjak <martin.roznovjak@gmail.com>
2018-10-11 15:36:20 +0300

    Added static directory for MS Office installation.
    Some middleware is causing issues - commented out

--- vm_setup/vm_setup/settings.py
+++ vm_setup/vm_setup/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from django.conf.global_settings import STATICFILES_DIRS
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -45,8 +48,8 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # 'django.middleware.csrf.CsrfViewre Web ClientMiddleware',
+    # 'django.contrib.auth.middleware.re Web ClientAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -120,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '../../MS_Office'),
+]
