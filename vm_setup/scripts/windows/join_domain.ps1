3967c1f3c019dc31587941ed1b3716fee0e3bf44
Martin Roznovjak <martin.roznovjak@gmail.com>
2018-10-15 15:47:05 +0300

    Formatting

--- vm_setup/scripts/windows/join_domain.ps1
+++ vm_setup/scripts/windows/join_domain.ps1
@@ -1,6 +1,6 @@
 function join_domain {
     param (
-	[Parameter(Mandatory=$true)] [string]$domain
+        [Parameter(Mandatory=$true)] [string]$domain
     )
 
    Add-Computer -DomainName $domain -Restart -Force
