580759269937b728e18e13e9c843b1a53e6e4fa2
Martin Roznovjak <martin.roznovjak@gmail.com>
2018-10-15 15:44:45 +0300

    Making functions out of the rest of the scripts

--- vm_setup/scripts/windows/change_ip.ps1
+++ vm_setup/scripts/windows/change_ip.ps1
@@ -1,14 +1,26 @@
-$HOST_NUM = ''
-$DNS_SERVER = ''
+function change_ip {
+    param (
+        [Parameter(Mandatory=$true)] [string]$ip,
+        [Parameter(Mandatory=$true)] [string]$subnet,
+        [string[]]$gateway,
+        [string[]]$dns
+    )
 
-$interface = Get-WmiObject win32_networkadapterconfiguration -filter "ipenabled = 'true'"
+    $net_adapter = Get-WmiObject win32_networkadapterconfiguration -filter "ipenabled = 'true'"
 
-if (-Not ($interface -is [System.Management.ManagementBaseObject])) {
-   echo "Wrong type, perhaps there are more interfaces, exiting"
-   exit 1
+    if (-Not ($net_adapter -is [System.Management.ManagementBaseObject])) {
+        echo "Wrong type of network adapter, perhaps there are more interfaces, exiting"
+        exit 1
+    }
+
+    $net_adapter.EnableStatic($ip, $subnet)
+    #Sleep -Seconds 5
+
+    if ($gateway -ne $null) {
+        $net_adapter.SetGateways($gateway)
+    }
+
+    if ($dns -ne $null) {
+        $net_adapter.SetDNSServerSearchOrder($dns)
+    }
 }
-
-$interface.EnableStatic("10.114.48.$HOST_NUM", "255.255.255.0")
-Sleep -Seconds 5
-$interface.SetGateways("10.114.48.1", 1)
-$interface.SetDNSServerSearchOrder("$DNS_SERVER")
