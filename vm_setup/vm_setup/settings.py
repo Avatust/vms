5bf55024c4f45812717ed3a7d6caaf5c8a580b79
Martin Roznovjak <martin.roznovjak@gmail.com>
2018-10-16 16:13:36 +0300

    Installing log_server and making migrations

--- vm_setup/vm_setup/settings.py
+++ vm_setup/vm_setup/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'log_server.apps.LogServerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
