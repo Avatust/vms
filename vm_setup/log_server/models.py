90ab9c50d0318f1af3ca50b106f7e3bcbf1a4429
Martin Roznovjak <martin.roznovjak@gmail.com>
2018-10-16 16:11:52 +0300

    Creating model for logging, registering in admin

--- vm_setup/log_server/models.py
+++ vm_setup/log_server/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class LogEntry(models.Model):
+    set_number = models.IntegerField()
+    machine_os = models.CharField(max_length=10)
+    message = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f'{self.machine_os} ({self.set_number}) at '
+            f'{self.created_at.strftime("%Y-%m-%d %H:%M:%S")}: '
+            f'{self.message}'
+        )
+
